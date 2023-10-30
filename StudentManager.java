d6a778856c8702e679fa5ff43a63649515bbb282
jasonJathanna <133443231+jasonJathanna@users.noreply.github.com>
2023-10-30 15:37:14 -0500

    Update StudentManager.java
    Edited method getGrades

--- StudentManager.java
+++ StudentManager.java
@@ -69,23 +69,10 @@ public class StudentManager {
 Scanner reader = new Scanner(System.in);
 
 	    System.out.println();
-	    System.out.println( students.get(index).getGrade());
+	    System.out.println( students.get(index).getGrade() + "%" );
 	    reader.netInt();
 	    homePage();
-		int x, back;
-		String[] students = {"dude", "other dude", "random russian", "unknown", "classified"};//random list of names until we have some
-		int[] grades = {7, 69, 420, 49, 87};//random list of grade until we have input
-	for (x=0; x<=(students.length-1); x++) {
-			
-			System.out.print(students[x]);
-			System.out.println(" "  + grades[x] + "%");
-	System.out.println("enter any number to go back");
-	back = reader.nextInt();
-	if (back <= 0 || back >= 0) {
-		   homePage();
-	}
-	
-	}
+		
     }
 
     public void seeTardies(){
