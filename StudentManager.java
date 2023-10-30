40f4b23782cea9bdf8e0be86578d30074ff0e652
Yoy0006 <149012342+Yoy0006@users.noreply.github.com>
2023-10-30 14:52:36 -0500

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -1,6 +1,6 @@
 
 public class StudentManager {
-
+import java.util.Scanner;
     public void generateStudents(){
 
     }
@@ -21,7 +21,26 @@ public class StudentManager {
 
     }
     public void markStudentAbsentOrPresent(){
-// 
+//Ask to choose two options 
+        System.out.println("Enter\n1)Absent\n2)Present\n3)Return to Homepage");
+
+//loop to repeat incase of wrong input
+       while (){ 
+// switch two options switch attend
+        switch(attend){
+            case 1: students.get(index).markabsent();
+                //remember to return to homepage
+                break;
+            case 2: students.get(index).markepresent();
+                 //remember to return to homepage
+            case 3: students.homepage();
+                 //remember to return to homepage
+                break;
+            default:
+                System.out.println("Invalid input! Choose another option:");
+
+        }
+        }
     }
     public void changeStudentGrade(){
 
