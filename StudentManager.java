832de57168f784e1014367cacf19d97f1fe4460a
Trooper20163 <78177991+Trooper20163@users.noreply.github.com>
2023-10-30 14:52:24 -0500

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -11,14 +11,21 @@ public class StudentManager {
 
     public void seeGrades(){
 Scanner reader = new Scanner(System.in);
-		int x;
-		String[] students = {"dude", "other dude", "random russian", "unknown", "classified"};//random list until we have students
-		int[] grades = {7, 69, 420, 49, 87};//random list until we actually have grades inputed into this
+		int x, back;
+		String[] students = {"dude", "other dude", "random russian", "unknown", "classified"};
+		int[] grades = {7, 69, 420, 49, 87};
 	for (x=0; x<=(students.length-1); x++) {
 			
 			System.out.print(students[x]);
 			System.out.println(" "  + grades[x] + "%");
-			}
+	System.out.println("enter any number to go back");
+	back = reader.nextInt();
+	if (back <= 0 || back >= 0) {
+		//call the method to go back
+		// i dont know what it is or how to becuase im some what dumb
+	}
+	
+	}
     }
 
     public void seeTardies(){
