f885eb4f224cc58ac089493298ce9852b5cadaa7
jasonJathanna <133443231+jasonJathanna@users.noreply.github.com>
2023-10-30 15:24:56 -0500

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -21,25 +21,35 @@ import java.util.Scanner;
 
     }
     public void markStudentAbsentOrPresent(){
-//Ask to choose two options 
+
+        Scanner sc = new Scanner(System.in);
+        //Ask to choose two options 
         System.out.println("Enter\n1)Absent\n2)Present\n3)Return to Homepage");
 
 //loop to repeat incase of wrong input
-       while (){ 
+       
 // switch two options switch attend
-        switch(attend){
-            case 1: students.get(index).markabsent();
+        switch(sc.nextInt()){
+            case 1: 
+                students.get(index).markAbsent();
+                homePage();
                 //remember to return to homepage
                 break;
-            case 2: students.get(index).markepresent();
+            case 2: 
+                students.get(index).markePresent();
+                homePage();
+                break;
                  //remember to return to homepage
-            case 3: students.homepage();
+            case 3: 
+                homePage();
                  //remember to return to homepage
                 break;
             default:
                 System.out.println("Invalid input! Choose another option:");
+                markStudentAbsentOrPresent();
+                
 
-        }
+        
         }
     }
     public void changeStudentGrade(){
