d61c59a4b90aa3e76695c5f89b286b2ebb68e478
Josiesullivan <149011913+Josiesullivan@users.noreply.github.com>
2023-10-30 14:03:34 -0500

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -6,7 +6,31 @@ public class StudentManager {
     }
 
     public void homePage(){
-
+        System.out.println("Enter \n 1. to exit \n 2. to see grades \n 3. to see tardies \n 4 to see classes \n 5. to take attendence \n 6. to change grades \n 7. for the next day")
+            int input = reader.nextInt();
+            switch (input) {
+                case 1:
+                    break;
+                case 2:
+                    seeGrades();
+                    break;
+                case 3:
+                    seeTardies();
+                    break;
+                case 4:
+                    seeClasses();
+                    break;
+                case 5: 
+                    markStudentAbsentOrPresent();
+                    break;
+                case 6:
+                    changeStudentGrade();
+                    break;
+                case 7:
+                    refresh();
+                    break;
+                default:
+                    System.out.println("This is not a valid entry. Enter \n 1. to exit \n 2. to see grades \n 3. to see tardies \n 4 to see classes \n 5. to take attendence \n 6. to change grades \n 7. for the next day")
     }
 
     public void seeGrades(){
