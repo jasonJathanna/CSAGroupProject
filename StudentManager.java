7944f4c0d4271cec4981721f4fc0d4024f1c3dcc
jasonJathanna <133443231+jasonJathanna@users.noreply.github.com>
2023-10-30 15:31:28 -0500

    Update StudentManager.java
    Added recursion to home page method

--- StudentManager.java
+++ StudentManager.java
@@ -60,7 +60,8 @@ public class StudentManager {
                     refresh();
                     break;
                 default:
-                    System.out.println("This is not a valid entry. Enter \n 1. to exit \n 2. to see grades \n 3. to see tardies \n 4 to see classes \n 5. to take attendance \n 6. to change grades \n 7. for the next day")
+                    System.out.println("This is not a valid entry.")
+			    homePage();
     }
 
     public void seeGrades(){
