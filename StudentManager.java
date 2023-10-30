aa79ef9a17dcb1cc5eaff3cedfeeae1fee5b0081
Trooper20163 <78177991+Trooper20163@users.noreply.github.com>
2023-10-30 14:47:53 -0500

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -10,7 +10,15 @@ public class StudentManager {
     }
 
     public void seeGrades(){
-
+Scanner reader = new Scanner(System.in);
+		int x;
+		String[] students = {"dude", "other dude", "random russian", "unknown", "classified"};//random list until we have students
+		int[] grades = {7, 69, 420, 49, 87};//random list until we actually have grades inputed into this
+	for (x=0; x<=(students.length-1); x++) {
+			
+			System.out.print(students[x]);
+			System.out.println(" "  + grades[x] + "%");
+			}
     }
 
     public void seeTardies(){
