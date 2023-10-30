f877693907b5f308c3c88bcd837b82983fe54e5e
jasonJathanna <133443231+jasonJathanna@users.noreply.github.com>
2023-10-30 14:31:40 -0500

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -2,7 +2,35 @@
 public class StudentManager {
 
     public void generateStudents(){
-
+         Scanner sc = new Scanner(System.in);
+	 System.out.println("do you want to add a student 0 for no 1 for yes");
+	 int input = sc.nextInt();
+	 
+	 if(input == 1) {
+		 students.add(new Student());
+		 System.out.println("enter in their id");
+		 students.get(students.size()-1).setId(sc.nextInt());
+		 System.out.println("enter in the four classes they are takeing");
+		 students.get(students.size()-1).setClasses( students.get(students.size()-1).getClasses().add(sc.nextInt()));
+		 students.get(students.size()-1).setClasses( students.get(students.size()-1).getClasses().add(sc.nextInt()));
+		 students.get(students.size()-1).setClasses( students.get(students.size()-1).getClasses().add(sc.nextInt()));
+		 students.get(students.size()-1).setClasses( students.get(students.size()-1).getClasses().add(sc.nextInt()));
+		 students.get(students.size()-1).setGrade(0);
+		 
+		 generateStudents();
+		 
+		 
+		
+		 
+	 } else if(input == 0) {
+		 
+	 } else  {
+		 System.out.println("error invalid input try again ");
+		 generateStudents();
+		 
+	 }
+ 
+ 
     }
 
     public void homePage(){
