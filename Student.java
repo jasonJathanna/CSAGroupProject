da2e5bc13c047b12395ba7c57cf718de02851db6
nenseyjathanna <nenseyjason03@gmail.com>
2023-10-29 19:11:48 -0500

    this fixes the JDK issue and adds templates for all of the methods which will be added. also sets up the student class

--- Student.java
+++ Student.java
@@ -1,9 +1,67 @@
 import java.util.ArrayList;
 
 public class Student {
-boolean hasBeenMarked;
-ArrayList<Boolean> attendence = new ArrayList<>();
-int id;
-int grade;
+
+
+    private boolean hasBeenMarked;
+    private ArrayList<Boolean> attendence = new ArrayList<>();
+
+    private ArrayList<String> classes = new ArrayList<>();
+    private int id;
+    private int grade;
+
+    public void markAbsent(){
+        attendence.add(false);
+    }
+
+    public void markPresent(){
+        attendence.add(true);
+    }
+
+
+
+
+
+    public boolean isHasBeenMarked() {
+        return hasBeenMarked;
+    }
+
+    public void setHasBeenMarked(boolean hasBeenMarked) {
+        this.hasBeenMarked = hasBeenMarked;
+    }
+
+    public ArrayList<Boolean> getAttendence() {
+        return attendence;
+    }
+
+    public void setAttendence(ArrayList<Boolean> attendence) {
+        this.attendence = attendence;
+    }
+
+    public ArrayList<String> getClasses() {
+        return classes;
+    }
+
+    public void setClasses(ArrayList<String> classes) {
+        this.classes = classes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+
 
 }
--- StudentManager.java
+++ StudentManager.java
@@ -1,4 +1,37 @@
 
 public class StudentManager {
 
+    public void generateStudents(){
+
+    }
+
+    public void homePage(){
+
+    }
+
+    public void seeGrades(){
+
+    }
+
+    public void seeTardies(){
+
+    }
+
+    public void seeClasses(){
+
+    }
+    public void markStudentAbsentOrPresent(){
+
+    }
+    public void changeStudentGrade(){
+
+    }
+
+    public void refresh(){
+
+    }
+
+
+
+
 }
