18ec9dff134fe92ad48efff80d53a93083c05f5a
Yoy0006 <149012342+Yoy0006@users.noreply.github.com>
2023-11-01 06:54:56 -0500

    Update StudentManager.java

--- StudentManager.java
+++ StudentManager.java
@@ -2,31 +2,37 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class StudentManager {
+//Define variables
     int index;
     ArrayList<Student> students = new ArrayList<>();
-
+// method adds new students to system
     public void generateStudents() {
         Scanner sc = new Scanner(System.in);
+//Ask if want to create new profiles 
         System.out.println("Do you want to add a student? Enter...\n0)No\n1)Yes");
         int input = sc.nextInt();
-
+//if yes, add on to students array list
         if (input == 1) {
             students.add(new Student());
+//create personalized student id
             System.out.println("Enter student id");
+// assign it to tha specific student
             students.get(students.size() - 1).setId(sc.nextInt());
+//Set schedule
             System.out.println("Enter in class codes for their current four classes:");
-
             for (int i = 0; i < 4; i++) {
                 students.get(students.size() - 1).addClass(sc.nextInt());
             }
+// initialize grades?
              students.get(students.size() - 1).setGrade(0);
 
             generateStudents();
 
-
-        } else if (input == 0) {
-
-        } else {
+        } 
+//ELSE what?
+        else if (input == 0) {}
+// if input not valid recall method until valid
+        else {
             System.out.println("Error! Invalid input, try again.");
             generateStudents();
 
@@ -34,7 +40,7 @@ public class StudentManager {
 
 
     }
-
+//match indexto id(find correct id)?
     public void setIndex() {
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter the student's ID number");
@@ -49,52 +55,65 @@ public class StudentManager {
         }
     }
 
+//create homepage method
+//contains all methods to any user action
     public void homePage() {
         Scanner reader = new Scanner(System.in);
+//Enter number based on what to edit or view
         System.out.println("Enter...\n 1.Exit \n 2.See grades \n 3.See tardies \n 4.See classes \n 5.Take attendance \n 6.Change grades \n 7. For the next day");
         int input = reader.nextInt();
         switch (input) {
+//Exit
             case 1:
                 break;
             case 2:
+//call method to View grade point average
                 setIndex();
                 seeGrades();
                 break;
+//call method to view tardies
             case 3:
                 setIndex();
                 seeTardies();
                 break;
+//call method to view class
             case 4:
                 setIndex();
                 seeClasses();
                 break;
+// call method to Edit present or absent
             case 5:
                 setIndex();
                 markStudentAbsentOrPresent();
                 break;
+//call method to Edit grade
             case 6:
                 setIndex();
                 changeStudentGrade();
                 break;
+//refersh, sets to next day
             case 7:
                 refresh();
                 break;
+//Returns to homepage 
             default:
                 System.out.println("Invalid input!");
                 homePage();
         }
     }
-
+//view percentage based arraylist average
     public void seeGrades() {
         Scanner reader = new Scanner(System.in);
-        System.out.println("enter 0 to go back");
+//Exit options
+        System.out.println("Enter 0 to go back");
+//calculate percent grade from number grade
         System.out.println(students.get(index).getGrade() + "%");
         reader.nextInt();
 //Return to homepage
         homePage();
 
     }
-
+//Change to sting?
     public void seeTardies() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 0 to go back");
@@ -103,31 +122,29 @@ public class StudentManager {
 //Return to homepage
         homePage();
     }
-
+//View list of classes
     public void seeClasses() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter 0 to go back");
+// Change to string and display
         System.out.println(students.get(index).getClasses().toString());
         sc.nextInt();
 //Return to homepage
         homePage();
     }
-
+//Mark absent or present
     public void markStudentAbsentOrPresent() {
-
         Scanner sc = new Scanner(System.in);
-        //Ask to choose two options
         System.out.println("Enter...\n1)Absent\n2)Present\n3)Return to Homepage");
-
-//loop to repeat incase of wrong input
-
 // switch two options switch attend
         switch (sc.nextInt()) {
+//Call markAbsent function
             case 1:
                 students.get(index).markAbsent();
 //Return to homepage
                 homePage();
                 break;
+//Call markPresent function
             case 2:
                 students.get(index).markPresent();
 //Return to homepage
@@ -139,7 +156,7 @@ public class StudentManager {
                 break;
             default:
                 System.out.println("Invalid input! Choose another option:");
-//If input is invalid, recall method to wnter new inout
+//If input is invalid, recall method to enter new input
                 markStudentAbsentOrPresent();
 
 
@@ -154,7 +171,7 @@ public class StudentManager {
 //Return to homepage
             homePage();
         }
-
+//CHANGE TO GPA
         if (changeGradeInput == 1) {
             System.out.println("Enter student's grade:");
             int newGrade = reader.nextInt();
