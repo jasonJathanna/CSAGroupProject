6629c1e0efbce1ba9ce5b91c4ce08363c7631b12
jasonJathanna <133443231+jasonJathanna@users.noreply.github.com>
2023-11-03 14:36:39 -0500

    Update Student.java
    new ver 1

--- Student.java
+++ Student.java
@@ -1,86 +1,49 @@
 import java.util.ArrayList;
 
 public class Student {
+//define variables/methods
 
-	 private boolean hasBeenMarked;
-	    private ArrayList<AttendenceState> attendence = new ArrayList<>();
-	    private ArrayList<Integer> grades = new ArrayList<>();
-	    private ArrayList<Integer> classes = new ArrayList<>();
-	    private int id;
-	    private int grade;
-	    
+    private boolean hasBeenMarked;
+    private ArrayList<Boolean> attendence = new ArrayList<>();
+    private ArrayList<Integer> grades = new ArrayList<>();
+    private ArrayList<Integer> classes = new ArrayList<>();
+    private int id;
+    private int grade;
+    
 
-	    public void markAbsent(){
-	        if(hasBeenMarked){
-	           System.out.println("attendece for this students was already compleated"); 
-	        } else{
-	        attendence.add(AttendenceState.absent);
-	        hasBeenMarked = true;
-	        }
-	    }
+    public void markAbsent(){
+        if(hasBeenMarked){
+           System.out.println("Attendece for this students was already compleated"); 
+        } else{
+        attendence.add(false);
+        hasBeenMarked = true;
+        }
+    }
 
-	    public void markPresent(){
-	       if(hasBeenMarked){
-	           System.out.println("attendece for this students was already compleated"); 
-	        } else{
-	        attendence.add(AttendenceState.present);
-	        hasBeenMarked = true;
-	        }
-	        
-	    }
+    public void markPresent(){
+       if(hasBeenMarked){
+           System.out.println("Attendece for this students was already compleated"); 
+        } else{
+        attendence.add(true);
+        hasBeenMarked = true;
+        }
+        
+    }
+    
+    public void addGrade(int newGrade) {
+    	grades.add(newGrade);
+    }
+    public ArrayList<Integer> getGrades() {
+		return grades;
+	}
 
-	    public void addClass(int classID){
-	        classes.add(classID);
-	    }
+	public void setGrades(ArrayList<Integer> grades) {
+		this.grades = grades;
+	}
 
-
-
-
-
-	    public boolean isHasBeenMarked() {
-	        return hasBeenMarked;
-	    }
-
-	    public void setHasBeenMarked(boolean hasBeenMarked) {
-	        this.hasBeenMarked = hasBeenMarked;
-	    }
-
-	    public ArrayList<AttendenceState> getAttendence() {
-	        return attendence;
-	    }
-
-	    public void setAttendence(ArrayList<AttendenceState> attendence) {
-	        this.attendence = attendence;
-	    }
-
-	    public ArrayList<Integer> getClasses() {
-	        return classes;
-	    }
-
-	    public void setClasses(ArrayList<Integer> classes) {
-	        this.classes = classes;
-	    }
-
-	    public int getId() {
-	        return id;
-	    }
-
-	    public void setId(int id) {
-	        this.id = id;
-	    }
-
-	    public int getGrade() {
-	        return grade;
-	    }
-
-	    public void setGrade(int grade) {
-	        this.grade = grade;
-	    }
-
-}
-
-
-
+	public void addClass(int classID){
+        classes.add(classID);
+    }
 
     public boolean isHasBeenMarked() {
         return hasBeenMarked;
@@ -124,4 +87,3 @@ public class Student {
 
 
 
-}
